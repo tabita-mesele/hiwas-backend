9a769e4455568bba0404c0fd2192015d690c6abd
Aki <akinahombirukwg@gmail.com>
2026-07-02 13:44:34 -0700

    l

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+// const app = express();
 const app = express();
 
 /* =========================
